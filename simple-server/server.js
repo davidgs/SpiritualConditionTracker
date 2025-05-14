c396ea6b0e156342faf8b1cfdb9f44befd3d3f96
David G. Simmons <davidgs@users.noreply.github.com>
2025-05-14 00:46:59 +0000

    removing useless things

--- simple-server/server.js
+++
@@ -1,7 +0,0 @@
-/**
- * Deployment proxy for Spiritual Condition Tracker
- * This file is used by Replit's deployment system
- */
-
-// Simply require our deployment server
-require('../deployment-server.js');
