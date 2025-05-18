ed49181c661568c8d1496fb4c78133f5d80d4a21
santafen <39090148-santafen@users.noreply.replit.com>
2025-05-18 23:49:44 +0000

    Restored to 'a60aa71c95d0818ff6d54901d27286167aa4a4ea'
    Replit-Restored-To: a60aa71c95d0818ff6d54901d27286167aa4a4ea

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ function Header({ title, menuOpen, setMenuOpen, isMobile }) {
             alignItems: 'center'
           }}>
             <img 
-              src={process.env.PUBLIC_URL + '/assets/logo.jpg'}
+              src="./logo.jpg"
               alt="App Logo" 
               style={{ 
                 width: '40px',
@@ -73,7 +73,7 @@ function Header({ title, menuOpen, setMenuOpen, isMobile }) {
           alignItems: 'center'
         }}>
           <img 
-            src={process.env.PUBLIC_URL + '/logo.jpg'}
+            src="./logo.jpg"
             alt="App Logo" 
             style={{ 
               width: '60px',
