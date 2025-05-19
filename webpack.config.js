ba42ae27f554b408fc087cbfc091de5eeed1e9db
David G. Simmons <davidgs@users.noreply.github.com>
2025-05-19 01:48:55 +0000

    Updated web pack config for native builds

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: './dist/',
+    publicPath: './',
     assetModuleFilename: 'assets/[hash][ext][query]'
   },
   module: {
