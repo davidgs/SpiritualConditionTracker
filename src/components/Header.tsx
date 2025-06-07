ec97544c8875f26c471d467422d3f693c6e7ed16
David G. Simmons <davidgs@users.noreply.github.com>
2025-06-07 22:25:56 +0000

    Make the log more round

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -51,7 +51,7 @@ function Header({ title, menuOpen, setMenuOpen, isMobile, onShowTour }) {
             width: '40px',
             height: '40px',
             objectFit: 'cover',
-            borderRadius: '10px'
+            borderRadius: '20px'
           }}
         />
         
