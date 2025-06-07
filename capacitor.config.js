14f40f488f08f6edac3ab92ec642901e65934865
David G. Simmons <davidgs@users.noreply.github.com>
2025-06-07 12:48:18 +0000

    Use absolute paths in Capacitor configs

--- capacitor.config.js
+++ capacitor.config.js
@@ -4,7 +4,7 @@
 module.exports = {
   appId: 'com.spiritualconditiontracker.app',
   appName: 'Spiritual Condition Tracker',
-  webDir: '/dist',
+  webDir: './dist',
   bundledWebRuntime: true,
   plugins: {
     CapacitorSQLite: {
